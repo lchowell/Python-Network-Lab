6558a45bdbd93bdf3f59d452c6355662f3b5fe05
Lance Howell <lancechowell@gmail.com>
2022-07-31 15:02:26 -0500

    added new line

--- Odd-Even-Challenge.py
+++ Odd-Even-Challenge.py
@@ -6,10 +6,13 @@
 x = 0
 
 # Get user input 
+print()
 x = int(input('Enter an odd or even number: '))
 
 # if the number user entered is even then the statement is true otherwise it is false, so it will run the 'odd' code block.
 if ((x%2) == 0):
+    print()
     print(f'{x} is even')
 else: 
+    print()
     print(f"{x} is odd")
