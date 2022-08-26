8d54a47ee2d7206eaee32ed5a87e42df18a709dd
Lance Howell <lancechowell@gmail.com>
2022-08-26 12:02:43 -0500

    New project started.

---
+++ Test-Python/lists.py
@@ -0,0 +1,13 @@
+# First list example
+# Indexes start with 0.
+
+edutainers = ['Adam', 'Aubri', 'Daniel', 'Jo', 'Justin', 'Zach']
+
+print('Edutainers: ', edutainers)
+print('The third edutainer is: ', edutainers[3])
+print('Edutainer 0: ', edutainers[0])
+
+# How do I know how many elements there are programmatically? Use "len()"
+
+print('The number of edutainers are: ', len(edutainers))
+print(edutainers[3], ' has ', len(edutainers[3]), 'characters.')
---
+++ Test-Python/multiple-varible.py
@@ -0,0 +1,5 @@
+hair_color = 'red'
+eye_color = 'blue'
+
+print("Justin's Hair Color: ", hair_color)
+print("Justin's Eye Color: ", eye_color)
